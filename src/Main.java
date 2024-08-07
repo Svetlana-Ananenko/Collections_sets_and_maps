d97b644418bae5cfde1cd3d6e3f0f9651a6c5370
Zver <SvetlanaEvcheko@yandex.ru>
2024-08-07 19:50:20 +0300

    hw_done_2

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         for (int i = 0; i < nums.size(); i++) {
             int a = nums.get(i) % 2;
-            if (a == 0) {
+            if (a != 0) {
                 System.out.println(nums.get(i));
             }
         }
@@ -26,8 +26,26 @@ public class Main {
         }
         System.out.println(numbers2);
 
+
+        System.out.println("_____Задача №2 второй вариант_____");
+        List<Integer> numbers5 = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        Collections.sort(numbers5);
+        int min = Integer.MIN_VALUE;
+        for (int a:numbers5)
+            if (a % 2 == 0 && a!=min) {
+                System.out.println(a);
+                min = a;
+            }
+
+
+
         System.out.println("_____Задача №3_____");
         Set<String> names = new HashSet<>(List.of("Георгий", "Света", "Алина", "Алина", "Роман", "Георгий"));
+        System.out.println(names);
+
+        System.out.println("_____Задача №3 второй вариант_____");
+        List<String> names2 = new ArrayList<>(List.of("Георгий", "Света", "Алина", "Алина", "Роман", "Георгий"));
+        Set<String> names3 = new HashSet<>(names2);
         System.out.println(names);
 
         System.out.println("_____Задача №4_____");
